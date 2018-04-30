c2691e4ce9869c79a40db5a382077567160198d7
Nauman93 <nauman@hotmail.it>
2018-04-30 17:27:24 +0200

    Working extension

---
+++ background.js
@@ -0,0 +1,12 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+	//url fo tabs to remove from chrome browser
+	var url = "https://www.google.it/*";
+	chrome.tabs.query({url: url}, function(tabs) {
+    
+    //cycle through all the tabs to remove them
+    for (var i = 0; i < tabs.length; i++) {
+    	chrome.tabs.remove(tabs[i].id);
+    }
+});
+});
